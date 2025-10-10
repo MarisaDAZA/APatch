7e82a6cb49b1ce570484425e8dee6681994eaee7
MarisaDAZA <1710978922@qq.com>
2025-10-10 11:50:02 +0100

    Fix Version Code to 11142

--- apd/build.rs
+++ apd/build.rs
@@ -10,12 +10,15 @@ fn get_git_version() -> Result<(u32, String), std::io::Error> {
         .output()?;
 
     let output = output.stdout;
+    let version_code = 11142;
+    /*
     let version_code = String::from_utf8(output).expect("Failed to read git count stdout");
     let version_code: u32 = version_code
         .trim()
         .parse()
         .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "Failed to parse git count"))?;
     let version_code = 10000 + 200 + version_code; // For historical reasons
+    */
 
     let version_name = String::from_utf8(
         Command::new("git")
